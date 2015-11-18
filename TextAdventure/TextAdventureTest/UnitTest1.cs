cbaca699b1f1de8b469462b5e752f3e3d4de2d05
Joey Frohlinger <jmafrohlinger@outlook.com>
2015-11-18 13:36:54 -0800

    Setting Up Project Structure

---
+++ TextAdventure/TextAdventureTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TextAdventureTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
